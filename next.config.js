cd55c425c70c97d5ffcae2c7e9d91cc86faa32d5
ZacharyLawPep <zacharylaw@peplink.com>
2024-03-08 09:33:33 +0800

    no image

--- next.config.js
+++ next.config.js
@@ -3,13 +3,11 @@ let assetPrefix = ''
 let basePath = ''
 if (process.env.GITHUB_ACTIONS || false) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
   assetPrefix = `/${repo}/`
   basePath = `/${repo}`
 }
-
 module.exports = {
   output:"export",
   assetPrefix: assetPrefix,
-  basePath: basePath,
+  basePath: basePath
 }
