9f800f221688b289d32ce6cb019934fe59fe9efa
ZacharyLawPep <zacharylaw@peplink.com>
2024-03-08 09:44:46 +0800

    before it worked

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
-let assetPrefix = ''
-let basePath = ''
-if (process.env.GITHUB_ACTIONS || false) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-module.exports = {
-  output:"export",
-  assetPrefix: assetPrefix,
-  basePath: basePath
-}
+const NextConfig = {
+  output: "export",
+};
+module.exports=NextConfig;
